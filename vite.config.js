6aa26c70ffbf1c5cd34a9a3ed1884fd5b788c783
cyndi-s <128951749+cyndi-s@users.noreply.github.com>
2025-11-06 20:18:16 -0500

    enable CI + Pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Use repo base on CI/Pages, '/' locally
+const isCI = !!process.env.GITHUB_ACTIONS
+
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: isCI ? '/react-todolist/' : '/',  // <-- change repo name if needed
 })
